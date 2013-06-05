98f35c9f94842d16f5b76b631cfc8a1e79196f55
Archie <brownarc@msu.edu>
2013-06-05 14:26:41 -0400

    Alphabetized Code

--- Periodic_Simulation.py
+++ Periodic_Simulation.py
@@ -1,12 +1,46 @@
 class Simulation (object):
     def __init__ (self, File_name):
+        self.Bins = []
+        self.Board = []
+        self.Data = []
         self.File_name = File_name
         self.Inventory = []
-        self.Bins = []
-        self.Data = []
         self.Virtual_Board = []
-        self.Board = []
+
         self.Extraction()
+
+
+    def Add_To_Board (self, Bin, Spot):
+        self.Board.append ([self.Bins[Bin][0]])
+        if Spot == 1:
+            self.Board [-1].append ("P")
+        elif Spot == 2:
+            self.Board [-1].append ("S")
+        else:
+            return "*****Error: Adding to Board Received Unexpected Results*****"
+
+
+    def Add_To_Virtual (self, Bin):
+        pass
+
+
+    def Empty_Bin (self, Emptied_Bin):                                                  #Configured by location of emptied bin in list
+        if self.Primary_Full(Emptied_Bin):
+            self.Bins[Emptied_Bin][1] = "S"
+            self.Bins[Emptied_Bin][2] = "E"
+            self.Add_To_Board (Emptied_Bin, 1)                                          #Need Lead Time Check
+
+        elif self.Primary_Is_Secondary (Emptied_Bin):                                   #need to put it on a vitual board
+            self.Bins[Emptied_Bin][1] = "E"
+            self.Add_To_Board (Emptied_Bin, 2)                                          #Need Lead Time Check
+            self.Stock_Out()
+
+        elif self.Secondary_Full(Emptied_Bin):
+            return "*****Error: Primary Emptied and Secondary Is Full*****"
+
+        elif self.Primary_Empty(Emptied_Bin) and self.Secondary_Empty(Emptied_Bin):
+            return "*****Error: Both Primary and Secondary Bin are Emptied.*****"
+
 
     def Extraction (self):
         import os
@@ -29,33 +63,9 @@ class Simulation (object):
             self.Inventory.append (element[0])
 
 
-    def Empty_Bin (self, Emptied_Bin):                                                  #Configured by location of emptied bin in list
-        if self.Primary_Full(Emptied_Bin):
-            self.Bins[Emptied_Bin][1] = "S"
-            self.Bins[Emptied_Bin][2] = "E"
-            self.Add_To_Board (Emptied_Bin, 1)                                          #Need Lead Time Check
-
-        elif self.Primary_Is_Secondary (Emptied_Bin):                                   #need to put it on a vitual board
-            self.Bins[Emptied_Bin][1] = "E"
-            self.Add_To_Board (Emptied_Bin, 2)                                          #Need Lead Time Check
-
-        elif self.Secondary_Full(Emptied_Bin):
-            return "*****Error: Primary Emptied and Secondary Is Full*****"
-
-        elif self.Primary_Empty(Emptied_Bin) and self.Secondary_Empty(Emptied_Bin):
-            return "*****Error: Both Primary and Secondary Bin are Emptied.*****"
-
-    def Add_To_Board (self, Bin, Spot):
-        self.Board.append ([self.Bins[Bin][0]])
-        if Spot == 1:
-            self.Board [-1].append ("P")
-        elif Spot == 2:
-            self.Board [-1].append ("S")
-        else:
-            return "*****Error: Adding to Board Received Unexpected Results*****"
-
-    def Add_To_Virtual (self, Bin):
+    def Lead_Time (self):
         pass
+    
 
     def Replenishment (self):                                                           # Need to run more tests in this area
         self.Check_Board()
@@ -80,31 +90,37 @@ class Simulation (object):
                 return "*****Error: Received Invalid Input ('P', 'S', 'E')*****"
 
         self.Board = []
+        
 
-            
-
-    def Lead_Time (self):
+    def Stock_Out (self):
         pass
 
-    #Various Error Checking       
+##Various Error Checking       
+
+    def Check_Board (self):                                                             #Will check both error conditions
+        pass
+
+        
+    def Primary_Empty (self, Bin_Location):                                             #Checks for Nothing to be in Primary Bin (it is Empty)
+        return self.Bins[Bin_Location][1] == "E"
+
 
     def Primary_Full (self, Bin_Location):                                              #Checks if Primary Bin is filled by Primary Bin
         return self.Bins[Bin_Location][1] == "P"
 
-    def Primary_Empty (self, Bin_Location):                                             #Checks for Nothing to be in Primary Bin (it is Empty)
-        return self.Bins[Bin_Location][1] == "E"
-
-    def Secondary_Full (self, Bin_Location):                                            #Checks if Secondary Bin is filled by a Secondary Bin
-        return self.Bins[Bin_Location][2] == "S"
-
-    def Secondary_Empty (self, Bin_Location):                                           #Checks if Seconday Bin is Empty
-        return self.Bins[Bin_Location][2] == "E"
     
     def Primary_Is_Secondary (self, Bin_Location):                                      #Checks if Primary Bin is filled by a Secondary Bin
         return self.Bins[Bin_Location][1] == "S"
 
-    def Check_Board (self):                                                             #Will check both error conditions
-        pass
-        
+
+    def Secondary_Full (self, Bin_Location):                                            #Checks if Secondary Bin is filled by a Secondary Bin
+        return self.Bins[Bin_Location][2] == "S"
+
+
+    def Secondary_Empty (self, Bin_Location):                                           #Checks if Seconday Bin is Empty
+        return self.Bins[Bin_Location][2] == "E"
+
+
+
         
     #Leadtime problem/triggering replenishments
